6938cd1d9d257bd5b3208026cc5f4f29c87be5f0
hellenjesus27-gif <hellenjesus27@gmail.com>
2026-09-22 14:04:01 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Sempre busca a versão mais nova da internet primeiro.
+// Só usa a cópia salva (cache) se estiver sem internet.
+const CACHE = 'hellen-financeiro';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((resp) => {
+        const copia = resp.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copia));
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
